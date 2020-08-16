63a07f24af212a1c14a264db5b331db0eb8ce77a
Aashish Thota <thotaaashish10@gmail.com>
2020-08-16 13:06:18 -0400

    initial commit 1

--- src/main/java/com/info/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/info/ecommerce/dao/ProductRepository.java
@@ -3,6 +3,7 @@ package com.info.ecommerce.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import com.info.ecommerce.entity.Product;
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
-	Page<Product> findByCategoryId(@RequestParam("id") Long id,Pageable pageable);
+	Page<Product> findByCategoryId(@Param("id") Long id,Pageable pageable);
 	
 	Page<Product> findByNameContaining(@RequestParam("name") String name,Pageable pageable);
 }
